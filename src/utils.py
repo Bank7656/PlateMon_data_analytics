719fe1f1c48fc969c9bbb5be065d818548c93a09
Thanapol Charoenphiphob <thacharo@Thanapols-MacBook-Pro.local>
2025-11-12 14:22:18 +0700

    add utils and color

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
+import sys
 import os
+import tomllib
 
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 from src.color import *
-
 
 def save_df_to_csv(df, dir, file_name):
 	
@@ -14,3 +18,9 @@ def save_df_to_csv(df, dir, file_name):
 	file_path = os.path.join(dir, file_name)
 	df.to_csv(file_path, index=False)
 	print(f"{OK} Data was saved sucessfully at {file_path}")
+
+def load_config(file_path):
+    """Loads a TOML configuration file."""
+    with open(file_path, 'rb') as f: # Open in binary read mode for tomllib
+        config = tomllib.load(f)
+    return config
